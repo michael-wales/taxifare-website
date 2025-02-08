8bd0468d7af721580ad2de4916e8c47909a2dea5
Michael Wales <104277040+michael-wales@users.noreply.github.com>
2025-02-08 16:58:29 -0500

    Revert "Added limits"
    This reverts commit 4817e5b0b9970319569bd268563fd55a48b2729f.

--- app.py
+++ app.py
@@ -3,14 +3,8 @@ import folium
 import geopy
 import requests
 
-# New York City's coordinates
-NYC_COORDS = (40.7128, -74.0060)
-
-valid_pickup, valid_dropoff = False, False
-
 st.title("NY taxi fare predictor")
 
-# Pickup and dropoff inputs as before
 pickup_date = st.date_input('Pickup date:')
 pickup_time = st.time_input('Pickup time:')
 
@@ -38,21 +32,16 @@ else:
 
 geolocator = geopy.geocoders.Nominatim(user_agent="taxifare-map-app")
 
-# Function to calculate distance from NYC
-def is_within_50_miles(lat, lon):
-    if lat is not None and lon is not None:
-        location_coords = (lat, lon)
-        distance = geopy.distance.geodesic(NYC_COORDS, location_coords).miles
-        if distance > 50:
-            return False, distance
-        return True, distance
-    return False, 0
-
 def create_map(pickup_latitude, pickup_longitude, dropoff_latitude, dropoff_longitude):
+
     m = folium.Map(location=[(pickup_latitude + dropoff_latitude) / 2, (pickup_longitude + dropoff_longitude) / 2], zoom_start=6)
+
     folium.Marker([pickup_latitude, pickup_longitude], popup="Pickup", icon=folium.Icon(color='blue')).add_to(m)
     folium.Marker([dropoff_latitude, dropoff_longitude], popup="Dropoff", icon=folium.Icon(color='red')).add_to(m)
+
     m.fit_bounds([[pickup_latitude, pickup_longitude], [dropoff_latitude, dropoff_longitude]])
+
+    # Return the map HTML to embed in Streamlit
     return m._repr_html_()
 
 # Geocode the addresses with retries in case of failure
@@ -73,15 +62,9 @@ if pickup_type == "Address" and pickup_address:
     pickup_latitude, pickup_longitude = geocode_with_retry(pickup_address)
     if pickup_latitude is None or pickup_longitude is None:
         st.error("Unable to geocode pickup address.")
-    else:
-        valid_pickup, distance = is_within_50_miles(pickup_latitude, pickup_longitude)
-        if not valid_pickup:
-            st.error(f"Pickup location is {distance:.2f} miles away from New York City but must be within 50 miles to calculate fare.")
 
 elif pickup_type == "Coordinates" and pickup_latitude and pickup_longitude:
-    valid_pickup, distance = is_within_50_miles(pickup_latitude, pickup_longitude)
-    if not valid_pickup:
-        st.error(f"Pickup coordinates are {distance:.2f} miles away from New York City but must be within 50 miles to calculate fare.")
+    pass  # Use the provided coordinates directly
 else:
     st.error("Please provide either an address or coordinates for pickup location.")
 
@@ -89,15 +72,9 @@ if dropoff_type == "Address" and dropoff_address:
     dropoff_latitude, dropoff_longitude = geocode_with_retry(dropoff_address)
     if dropoff_latitude is None or dropoff_longitude is None:
         st.error("Unable to geocode dropoff address.")
-    else:
-        valid_dropoff, distance = is_within_50_miles(dropoff_latitude, dropoff_longitude)
-        if not valid_dropoff:
-            st.error(f"Dropoff location is {distance:.2f} miles away from New York City but must be within 50 miles to calculate fare.")
 
 elif dropoff_type == "Coordinates" and dropoff_latitude and dropoff_longitude:
-    valid_dropoff, distance = is_within_50_miles(dropoff_latitude, dropoff_longitude)
-    if not valid_dropoff:
-        st.error(f"Dropoff coordinates are {distance:.2f} miles away from New York City but must be within 50 miles to calculate fare.")
+    pass  # Use the provided coordinates directly
 else:
     st.error("Please provide either an address or coordinates for dropoff location.")
 
@@ -105,7 +82,7 @@ passenger_count = st.number_input('How many passengers?', min_value=1, max_value
 
 url = 'https://taxifare-805490564375.europe-west1.run.app/predict'
 
-params = {
+params =  {
     "pickup_datetime": str(pickup_date) + ' ' + str(pickup_time),
     "pickup_longitude": pickup_longitude,
     "pickup_latitude": pickup_latitude,
@@ -116,7 +93,7 @@ params = {
 
 response = requests.get(url, params=params).json()
 
-if valid_pickup and valid_dropoff and 'fare' in response:
+if 'fare' in response:
     st.markdown(f'#### **Predicted fare:** ${round(response["fare"], 2)}')
 
 if pickup_latitude is not None and dropoff_latitude is not None:
